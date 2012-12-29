981daa045baeef94996b8bb3d8fb1db570285ce0
Yang, Chenjun <chenjun.yang@sap.com>
2012-12-29 17:21:57 +0800

    First commit
    Change-Id: I69b613b3bfa4722ca56b7c95f5e20653e3032643

---
+++ javademo/src/main/java/ycj/demo/MainApp.java
@@ -0,0 +1,14 @@
+package ycj.demo;
+
+public class MainApp
+{
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+    }
+
+}
